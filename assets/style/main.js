ebf229ba2fa4b8ef9cd788782aff9f02e605ac52
Nomaan240 <noommaa240@gmail.com>
2021-12-01 09:43:02 +0600

    complete this website.

---
+++ assets/style/main.js
@@ -0,0 +1,46 @@
+
+let toggle = document.querySelector("#header .toggle-button");
+
+let collapse = document.querySelectorAll("#header .collapse");
+
+toggle.addEventListener('click', function(){
+    collapse.forEach(col => col.classList.toggle("collapse-toggle"))
+});
+
+
+// swiper libary initialization start
+new Swiper('.swiper',{
+    direction:'horizontal',
+    loop:true,
+    slidesPerView:5,
+    autoplay:{
+        delay:3000
+    },
+    //responsive breakpoints start
+    breakpoints:{
+
+        '@0':{
+            slidesPerView:2
+        },
+
+        //888px
+        '@1.00':{
+            slidesPerView:3
+        },
+
+        //1110px
+        '@1.25':{
+            slidesPerView:4
+        },
+
+        //1330px
+        '@1.50':{
+            slidesPerView:5
+        }
+
+    }
+
+    //responsive breakpoints end
+
+});
+// swiper libary initialization end
